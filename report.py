ea611f5ff168fde5550be811e1d49a4f466512a6
Your Name <you@example.com>
2021-04-12 13:36:09 +0200

    report

---
+++ report.py
@@ -0,0 +1,29 @@
+# Enumerate() method adds a counter to an iterable and returns it in a form of enumerate object contain the counter an value.
+# name_list = ['sara' , 'hager']
+# name = "sara"
+# result1 = enumerate(name_list)
+# result2 = enumerate(name)
+# # print(result1)
+# # print(result2)
+# for name in result1 :
+#     print(name)
+# #output
+# # (0, 'sara')
+# # (1, 'hager')
+
+#2- lambda Exression is an anonymous fuction which mean a function without a name we can use it if we want to a function which it's nameless
+# for a short eriod of time which is commonly use to pass this function as an argument to another function 
+
+#3 - operator module
+import operator
+print(operator.add(1,2))
+print(operator.sub(4,2))
+print(operator.mul(2,2))
+print(operator.truediv(4,2))
+print(operator.pow(2,2))
+print(operator.mod(5,2))
+print(operator.le(2,2))
+print(operator.lt(4,2))
+print(operator.gt(4,2))
+print(operator.eq(2,2))
+
